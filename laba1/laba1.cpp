a0b71f998a332a8f69d4d8ca1024a7a9564bf750
User <User@DESKTOP-87C1B0S>
2025-09-19 20:33:30 +0300

    Добавлены функции для проверки int и float на минимальное и максимальное значения

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -1,26 +1,60 @@
-﻿#include <iostream>
-#include <fstream>
-#include <string>
-#include <limits>
+﻿#include <iostream>   // Для ввода-вывода (cin, cout)
+#include <fstream>    // Для работы с файлами (ifstream, ofstream)
+#include <string>     // Для работы со строками (string)
+#include <limits>     // Для numeric_limits (пределы числовых типов)
 
 using namespace std;
 
 struct Truba {
-	string name1;
+	string name;
 	float dlina_km;
 	int diameter_mm;
 	bool remont;
 };
 
 struct Compressor {
-	string name2;
+	string name;
 	int kol_cehov;
 	int vrabote;
-	float klass_stanccii;
+	float klass_stancii;
 };
+
+float Proverka_fl(float min_znach = 0.0f, float max_znach = numeric_limits<float>::max()) {
+	float znachenie;
+	while (true) {
+		cin >> znachenie;
+		if (cin.fail() || znachenie < min_znach || znachenie > max_znach) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Ошибка. Введите число от " << min_znach << " до " << max_znach << ": ";
+		}
+		else {
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			return znachenie;
+		}
+	}
+}
+
+int Proverka_in(int min_znach = 0, int max_znach = numeric_limits<int>::max()) {
+	int znachenie;
+	while (true) {
+		cin >> znachenie;
+		if (cin.fail() || znachenie < min_znach || znachenie > max_znach) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Ошибка. Введите число от " << min_znach << " до " << max_znach << ": ";
+		}
+		else {
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			return znachenie;
+		}
+	}
+}
+
 
 void menu() {
 	while (true) {
+		cout << "\n Меню \n";
 		cout << "1. Добавить трубу" << endl;
 		cout << "2. Добавить КС" << endl;
 		cout << "3. Просмотр всех объектов" << endl;
@@ -29,11 +63,13 @@ void menu() {
 		cout << "6. Сохранить данные " << endl;
 		cout << "7. Загрузить данные " << endl;
 		cout << "8. Выход" << endl;
-}
-
-	int main() {
-		setlocate(Truba, Compressor);
-		menu();
-		return 0;
 	}
+};
 
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    menu();
+    return 0;
+}
